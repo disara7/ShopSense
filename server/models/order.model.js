539e2800ba319ccdc930c0589b7dd59172b8a2b2
disara7 <129250994+disara7@users.noreply.github.com>
2024-04-24 12:32:59 +0530

    Update order.model.js

---
+++ server/models/order.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const orderModel = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    product: [
+        {
+            productId: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            }
+        }
+    ],
+    amount: {
+        type: Number,
+        required: true,
+    },
+    address: {
+        type: Object,
+        required: true,
+    },
+    status: {
+        type: String,
+        default: "Pending",
+    },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Order", orderModel);
