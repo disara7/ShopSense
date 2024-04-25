2ed1a2da1ab46e356c4d695ec457e3d23a7c5f58
disara7 <129250994+disara7@users.noreply.github.com>
2024-04-25 12:08:42 +0530

    better route function organization

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,76 +1,10 @@
+const { register, login } = require("../controllers/auth.controller");
+
 const router = require("express").Router();
-const User = require("../models/user.model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 
-router.post("/register", async (req, res) => {
-    try {
-        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
-        });
+router.post("/register", register);
 
-        await newUser.save();
-
-        const {password, ...info} = newUser._doc;
-        res.status(200).json({
-            message: "User created successfully!",
-            data: info,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "User creation failed!",
-            error: error,
-        });
-    }
-});
-
-router.post("/login", async (req, res)=>{
-
-    try {
-
-        const user = await User.findOne({email: req.body.email});
-        if (!user) {
-            return res.status(404).json({
-                message: "Email does not exist"
-            })
-        }
-
-        const comparedPassword = await bcrypt.compare(req.body.password, user.password);
-        if (!comparedPassword) {
-            return res.status(404).json({
-                message: "Incorrect email or password"
-            })
-        }
-
-        const token = jwt.sign({
-            userId: user._id,
-            isAdmin: user.isAdmin,
-        }, process.env.JWT_KEY,
-        //set expiry date for JWT (5 days) 
-        // {
-        //     expiresIn:"5d"
-        // }
-        );
-
-        const { password, ...info} = user._doc;
-        res.status(200).json({
-            data: {...info, token},
-            message: "Login successful!",
-        });
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "Login failed",
-            error: error,
-        })  
-    }
-    
-});
+router.post("/login", login);
 
 module.exports = router;
