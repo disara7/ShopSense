a898b720071aa1c9ac3d2fab9fa5ad6d259193e2
disara7 <129250994+disara7@users.noreply.github.com>
2024-04-25 11:07:39 +0530

    Update auth.route.js
    hides password

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
     try {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         });
 
         await newUser.save();
+
+        const {password, ...info} = newUser;
         res.status(200).json({
             message: "User created successfully!",
-            data: newUser,
+            data: info,
         });
     } catch (error) {
         console.log(error);
