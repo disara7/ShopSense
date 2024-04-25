c44f0860613c79b279a6d120b00d058dbcc15250
disara7 <129250994+disara7@users.noreply.github.com>
2024-04-25 09:53:15 +0530

    Update auth.route.js

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,11 +4,22 @@ const User = require("../models/user.model");
 router.post("/register", async (req, res) => {
     try {
         const newUser = new User({
-            user //6:40 https://youtu.be/kG9bhjuk6H8?si=DhbDndadXpgHdnHI
-        })
-        
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        await newUser.save();
+        res.status(200).json({
+            message: "User created successfully!",
+            data: newUser,
+        });
     } catch (error) {
         console.log(error);
+        res.status(500).json({
+            message: "User creation failed!",
+            error: error,
+        });
     }
 });
 
