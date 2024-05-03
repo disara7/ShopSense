63adf474417d4591fd48b08047379d0df8120696
disara7 <129250994+disara7@users.noreply.github.com>
2024-05-03 17:17:18 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./user.route.js");
 const authRoutes = require("./auth.route.js");
+const productRoutes = require("./product.route.js");
 
 const base = "/api/v1";
 
 router.use(`${base}/users`, userRoutes);
 router.use(`${base}/auth`, authRoutes);
+router.use(`${base}/product`, productRoutes);
 
 
 module.exports = router;
